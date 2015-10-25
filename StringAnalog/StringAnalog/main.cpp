77a9b0ba76e36282b1b0af35533dea6b4b8dbfd0
DEVTYPE <Informatik.on.GitHub@gmail.com>
2015-10-26 02:10:19 +0600

    Added two square bracket overloading to String

--- StringAnalog/StringAnalog/main.cpp
+++ StringAnalog/StringAnalog/main.cpp
@@ -2,6 +2,8 @@
 //  main.cpp
 //  StringAnalog
 //
+//  https://stepic.org/lesson/Правила-переопределения-операторов-559/step/9?course=Программирование-на-языке-C  &unit=882
+//
 
 #include <iostream>
 #include <cstddef> // size_t
@@ -109,6 +111,27 @@ struct String {
     }
     /* ... */
     
+    String(const char *str, size_t len) {
+        size = len;
+        this->str = cpStr(str, size);
+    }
+    struct OperatorBracketHelper {
+        OperatorBracketHelper(char *str, size_t i) {
+            str_ = str;
+            i_ = i;
+        }
+        String operator []( size_t j ) const {
+            String s(str_ + i_, j-i_);
+            return s;
+        }
+        char *str_;
+        size_t i_;
+    };
+    OperatorBracketHelper operator []( size_t i ) const {
+        OperatorBracketHelper o(str, i);
+        return o;
+    }
+    
     size_t size;
     char *str;
 };
@@ -127,12 +150,41 @@ void test2()
 {
     String greet("Hello");
     char ch1 = greet.at(0); // в char ch1 = greet.at(0) будет вызвана не const версия метода at
+    printf("%c\n", ch1);
 
     String const const_greet("Hello, Const!");
     char const &ch2 = const_greet.at(0); // в char const & ch2 = const_greet.at(0) будет вызвана const версия метода at
+    printf("%c\n", ch2);
+}
+
+void test3()
+{
+    String const hello("hello");
+    printf("%s\n", hello.str);
+    
+    String const hell = hello[0][4]; // теперь в hell хранится подстрока "hell"
+    printf("%s\n", hell.str);
+    
+    String const ell  = hello[1][4]; // теперь в ell хранится подстрока "ell"
+    printf("%s\n", ell.str);
+
 }
 
 int main(int argc, const char * argv[]) {
-    test2();
+    test3();
     return 0;
 }
+
+
+/*
+ В этой задаче вам требуется реализовать оператор [] для уже известного вам класса String. Однако на этот раз оператор должен реализовывать нестандартное поведение: оператор нужно реализовать таким образом, чтобы для объекта str класса String можно было писать str[i][j] и это выражение возвращало подстроку начинающуюся в позиции i (считая с 0) и заканчивающуюся в позиции j (не включая).
+ 
+ Например:
+ 
+ String const hello("hello");
+ String const hell = hello[0][4]; // теперь в hell хранится подстрока "hell"
+ String const ell  = hello[1][4]; // теперь в ell хранится подстрока "ell"
+ 
+ Обратите внимание, что i может равняться j, в этом случае результатом должна быть пустая строка. Гарантируется, что i никогда не будет больше j, и они не будут выходить за пределы длины строки.
+ Требования к реализации: При выполнении задания вы можете создавать любые методы/конструкторы или даже структуры/классы, если они вам нужны. Реализовывать методы, которые уже объявленны в шаблоне, не нужно  они уже реализованы. При выполнении задания не вводите и не выводите что-либо. Реализовывать функцию main не нужно.
+*/
