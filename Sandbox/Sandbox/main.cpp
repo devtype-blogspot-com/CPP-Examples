d36610c68193f6d6c2d43628092340a4c6fcfc89
DEVTYPE <Informatik.on.GitHub@gmail.com>
2015-11-30 03:18:31 +0600

    Updated Sandbox project

--- Sandbox/Sandbox/main.cpp
+++ Sandbox/Sandbox/main.cpp
@@ -6,6 +6,7 @@
 //
 
 #include <iostream>
+#include <vector>
 
 
 #define square(x) (x * x)
@@ -45,7 +46,19 @@ void test4() {
 }
 
 
+void test5() {
+    int myints[] = {10,20,30,30,20,10,10,20};
+    std::vector<int> v(myints,myints+8);           // 10 20 30 30 20 10 10 20
+    std::sort (v.begin(), v.end());                // 10 10 10 20 20 20 30 30
+    std::vector<int>::iterator low,up;
+    low=std::lower_bound (v.begin(), v.end(), 20); //          ^
+    up= std::upper_bound (v.begin(), v.end(), 20); //                   ^
+    std::cout << "lower_bound at position " << (low- v.begin()) << '\n';
+    std::cout << "upper_bound at position " << (up - v.begin()) << '\n';
+}
+
+
 int main(int argc, const char * argv[]) {
-    test4();
+    test5();
     return 0;
 }
